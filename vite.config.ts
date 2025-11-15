5eb4329bece30d36d060c5a3036d9e7095c96895
mxrxxsv <mariusjamess26@gmail.com>
2025-11-15 14:39:59 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set the `base` to the repository name so assets load correctly on GitHub Pages
+  // If you publish to the root (username.github.io) set this to '/'
+  base: '/Mxrxxs-portfolio/',
   plugins: [react()],
 })
